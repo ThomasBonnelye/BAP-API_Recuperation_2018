dcd5a80d83be7d4a4bb98aab22b73c5616549402
ThomasBONNELYE <thomas.bonnelye@devinci.fr>
2018-12-05 11:30:58 +0100

    test without the final API

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Http} from "@angular/http";
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +9,27 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    url:string;
+    data:string;
+
+  constructor(public http: Http, public navCtrl: NavController) {
+
+  }
+
+  ionViewDidLoad(){
+      this.loadData();
+  }
+
+  loadData(){
+
+      this.http.get('https://randomuser.me/api/?results=50')
+          .map(res => res.json())
+          .subscribe(data => {
+              this.data = data.results;
+              console.log(data.results);
+          },err => {console.log(err);
+
+          });
 
   }
 
